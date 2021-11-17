3d5d721b697130a7fc9f2d6ec51bd20f16b606c9
Chester <cheung319@gmail.com>
2021-11-16 23:08:28 -0500

    commit tree 8ed311ea9dc8fdc05e9fa8d5106c34df97d48024

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+const names = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(names){
+    const newArray = []
+    for (let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray
+}
+
+function countDown(){ 
+    let i = 10;
+    while(i > -1){ 
+        console.log(i--);  
+    } 
+}
